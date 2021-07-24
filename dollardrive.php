281a96de3e4ac1471aeb7b2eedc435b7d95cbbcb
dollarstir <kpin463@gmail.com>
2021-07-24 14:33:16 +0000

    first comit

---
+++ dollardrive.php
@@ -0,0 +1,18 @@
+<?php
+// Code compiled by CEO of dollarsoft 
+// 0556676471
+// kpin463@gmail.com
+// www.dollarstir.com /www.dollarstir.tk
+function page($id)
+{
+    switch ($id) {
+        case 1:
+            echo 'Home - Tucee Institute of Counseling';
+            break;
+        case 2:
+            echo '';
+            break;
+        default:
+            echo 'Unknown';
+    }
+}
